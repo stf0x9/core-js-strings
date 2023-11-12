69922900dd8fc236fa1f62d7d16a640c6d5c7df5
stf0x9 <stf0x9@gmail.com>
2023-11-12 07:32:07 -0800

    feat: implement repeatString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -133,8 +133,11 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (times < 0) {
+    return str.repeat(0);
+  }
+  return str.repeat(times);
 }
 
 /**
