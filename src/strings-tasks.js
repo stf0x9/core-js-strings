6ef567c160ba0c8b5bf946cb29c6190fbb0b201a
stf0x9 <stf0x9@gmail.com>
2023-11-12 11:44:18 -0800

    feat: imlement extractNameFromTemplate

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -444,8 +444,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const [firstName, lastName] = value.substring(7).replace('!', '').split(' ');
+  return `${firstName} ${lastName}`;
 }
 
 /**
