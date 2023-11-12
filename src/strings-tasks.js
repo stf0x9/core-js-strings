e285a0b0b6bd4136cb67567f5743db0d133b7203
stf0x9 <stf0x9@gmail.com>
2023-11-12 09:17:07 -0800

    feat: implement formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -247,8 +247,11 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  return minutes
+    .toString()
+    .padStart(2, '0')
+    .concat(seconds.toString().padStart(3, ':0'));
 }
 
 /**
