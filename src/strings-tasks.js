726d5d696b92520d4f82924c8a0b2b19764f3144
stf0x9 <stf0x9@gmail.com>
2023-11-12 08:47:23 -0800

    feat: implement removeLastOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -172,8 +172,12 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const index = str.lastIndexOf(value);
+  if (index === -1) {
+    return str;
+  }
+  return str.slice(0, index) + str.slice(index + value.length);
 }
 
 /**
