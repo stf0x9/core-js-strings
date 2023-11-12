2ed6ebdb4e9af481e3516354598a221860b28c5e
stf0x9 <stf0x9@gmail.com>
2023-11-12 10:50:43 -0800

    feat: implement reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -383,8 +383,13 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const arr = str.split(' ');
+  const accumulator = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    accumulator.push(arr[i].split('').reverse().join(''));
+  }
+  return accumulator.join(' ');
 }
 
 /**
